ac1983f70de975659ec3b036fac44870ede52216
jcagigas <jose.maria.cagigas@gmail.com>
2023-04-11 20:14:17 +0200

    confi micro openapi para swagger

--- microservicios-usuarios/src/main/java/com/practice/app/usuarios/controllers/AlumnoController.java
+++ microservicios-usuarios/src/main/java/com/practice/app/usuarios/controllers/AlumnoController.java
@@ -4,13 +4,17 @@ import com.practice.app.usuarios.models.dto.AlumnoRequestDto;
 import com.practice.app.usuarios.models.entity.Alumno;
 import com.practice.app.usuarios.services.AlumnoConverter;
 import com.practice.app.usuarios.services.AlumnoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+@Tag(name = "Alumno API", description = "This APi serve all functionality for management Alumnos")
 @RestController
 public class AlumnoController {
 
@@ -20,10 +24,19 @@ public class AlumnoController {
     @Autowired
     private AlumnoConverter converter;
 
+    @Operation(description = "Return all alumnos bundled into Response", summary ="Return 204 if no data found")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Exito: Listado de alumnos."),
+            @ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping
     public ResponseEntity<?> listar(){
         return ResponseEntity.ok().body(service.findAll());
     }
+
+    @Operation(description = "Return alumno by id bundled into Response")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Alumno"),
+            @ApiResponse(responseCode = "500", description = "Internal error")})
     @GetMapping("/{id}")
     public ResponseEntity<?> ver(@PathVariable Long id){
         Optional<Alumno> o = service.findById(id);
@@ -33,6 +46,10 @@ public class AlumnoController {
         return ResponseEntity.ok(converter.convertAlumnoToAlumnoResponseDto(o.get()));
     }
 
+    @Operation(description = "Return alumno updated bundled into Response")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Alumno"),
+            @ApiResponse(responseCode = "500", description = "Internal error")})
     @PostMapping
     public ResponseEntity<?> crear(@RequestBody AlumnoRequestDto alumnoRequestDto){
         Alumno alumno = converter.convertAlumnoRequestDtotoAlumno(alumnoRequestDto);
@@ -40,6 +57,10 @@ public class AlumnoController {
         return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDB);
     }
 
+    @Operation(description = "Return alumno updated")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Alumno updated"),
+            @ApiResponse(responseCode = "500", description = "Internal error")})
     @PutMapping("/{id}")
     public ResponseEntity<?> editar(@RequestBody AlumnoRequestDto alumnoRequestDto, @PathVariable Long id){
 
@@ -54,6 +75,10 @@ public class AlumnoController {
 
     }
 
+    @Operation(description = "Return code 204")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",description = "Alumno deleted"),
+            @ApiResponse(responseCode = "500", description = "Internal error")})
     @DeleteMapping("/{id}")
     public ResponseEntity<?> delete(@PathVariable Long id){
 
