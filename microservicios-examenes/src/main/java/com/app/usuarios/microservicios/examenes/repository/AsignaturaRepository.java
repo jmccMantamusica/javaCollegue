4d8216b973bff31c1bb461e014636b39c38e5966
jcagigas <jose.maria.cagigas@gmail.com>
2023-04-17 11:39:51 +0200

    quitamos zuul y ponemos gateway v.1

--- microservicios-examenes/src/main/java/com/app/usuarios/microservicios/examenes/repository/AsignaturaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.usuarios.microservicios.examenes.repository;
-
-import com.app.usuarios.microservicios.commons.models.entity.Asignatura;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface AsignaturaRepository extends PagingAndSortingRepository<Asignatura, Long> {
-}
